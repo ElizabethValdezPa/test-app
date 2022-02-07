df5f378b4d96a402ad8f24e2d88a045458f22075
Liss25 <0332152033@unjfsc.edu.pe>
2022-02-06 21:30:56 -0500

    carga de datos

--- src/main/java/com/prueba/service/ProcesarArchivosServiceImpl.java
+++ src/main/java/com/prueba/service/ProcesarArchivosServiceImpl.java
@@ -4,6 +4,7 @@ import com.prueba.model.AdditionalData;
 import com.prueba.model.Channel;
 import com.prueba.model.DataModel;
 import org.apache.commons.io.FileUtils;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.springframework.stereotype.Service;
@@ -18,27 +19,31 @@ public class ProcesarArchivosServiceImpl implements ProcesarArchivosService {
 
     @Override
     public DataModel leerArchivos(String id) throws Exception {
-        JSONObject jsonObject = (JSONObject) readJsonObject();
-        DataModel dataModel = new DataModel();
-        dataModel.setId((String) jsonObject.get("id"));
-        dataModel.setHref((String) jsonObject.get("href"));
-        dataModel.setCorrelationId((String) jsonObject.get("correlationId"));
-        dataModel.setDescription((String) jsonObject.get("description"));
-        dataModel.setCreationDate((String) jsonObject.get("creationDate"));
-        dataModel.setCategory((String) jsonObject.get("category"));
-        dataModel.setSource((String) jsonObject.get("source"));
-        dataModel.setStatus((String) jsonObject.get("status"));
-        dataModel.setStatusChangeDate((String) jsonObject.get("statusChangeDate"));
-        dataModel.setRelatedParty((String) jsonObject.get("relatedParty"));
-        dataModel.setRelatedObject((String) jsonObject.get("relatedObject"));
-        dataModel.setNote((String) jsonObject.get("note"));
-        dataModel.setChannel((List<Channel>) jsonObject.get("channel"));
-        dataModel.setAdditionalData((List<AdditionalData>) jsonObject.get("additionalData"));
+        JSONObject ss = (JSONObject) readJsonObject();
 
-        if(id.equals(dataModel.getId()) || id.isEmpty()){
-            return dataModel;
+        JSONArray jsonArray = (JSONArray) ss.get("data");
+        DataModel dataModel = new DataModel();
+
+        for (int i = 0; i <jsonArray.size(); i++) {
+            JSONObject jsonObject = (JSONObject) jsonArray.get(i);
+            if(jsonObject.get("id").toString().equals(id)) {
+                dataModel.setId((String) jsonObject.get("id"));
+                dataModel.setHref((String) jsonObject.get("href"));
+                dataModel.setCorrelationId((String) jsonObject.get("correlationId"));
+                dataModel.setDescription((String) jsonObject.get("description"));
+                dataModel.setCreationDate((String) jsonObject.get("creationDate"));
+                dataModel.setCategory((String) jsonObject.get("category"));
+                dataModel.setSource((String) jsonObject.get("source"));
+                dataModel.setStatus((String) jsonObject.get("status"));
+                dataModel.setStatusChangeDate((String) jsonObject.get("statusChangeDate"));
+                dataModel.setRelatedParty((String) jsonObject.get("relatedParty"));
+                dataModel.setRelatedObject((String) jsonObject.get("relatedObject"));
+                dataModel.setNote((String) jsonObject.get("note"));
+                dataModel.setChannel((List<Channel>) jsonObject.get("channel"));
+                dataModel.setAdditionalData((List<AdditionalData>) jsonObject.get("additionalData"));
+            }
         }
-        return null;
+        return dataModel;
 
     }
 
